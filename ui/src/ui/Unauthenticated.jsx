65796eade10454eaa4878565fc37937701b5a444
KaradzhovMino <mino.karadzhov@dreamix.eu>
2026-06-04 18:12:47 +0300

    Fixed missing logo from login page

--- ui/src/ui/Unauthenticated.jsx
+++ ui/src/ui/Unauthenticated.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { ContentContainer } from "../components";
+import { LogoWhite } from "@/components/icons";
 import GoogleLoginBtn from "./GoogleLoginButton";
 
 function Unauthenticated() {
   return (
     <div className="">
-      <ContentContainer className="flex h-screen items-center justify-center bg-dwc-background select-none">
+      <ContentContainer className="flex h-screen items-center justify-center bg-dec-background select-none">
         <div className="flex flex-col">
-          <EuroLogo className="w-60" />
+          <LogoWhite className="w-60" />
           <GoogleLoginBtn />
         </div>
       </ContentContainer>
